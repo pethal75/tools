6ac078f0c46e2e359a9a4224c6abd35590875951
haladp_cat <peter.halada@cat.com>
2024-11-08 13:44:55 +0100

    Added list of projects in header combo box

--- src/main/java/com/javaservices/tools/model/ToolsCustomProperties.java
+++ src/main/java/com/javaservices/tools/model/ToolsCustomProperties.java
@@ -1,10 +1,15 @@
 package com.javaservices.tools.model;
 
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@Data
 public class ToolsCustomProperties {
+    public static final String STORAGE_TYPE_FILE = "FILE";
+
     public String storageType;
+
     public String storageDirectory;
 
     public String lastProject;
--- src/main/java/com/javaservices/tools/web/beans/ProjectDetailBean.java
+++ src/main/java/com/javaservices/tools/web/beans/ProjectDetailBean.java
@@ -12,6 +12,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.primefaces.model.file.UploadedFile;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.javaservices.tools.model.ToolsCustomProperties.STORAGE_TYPE_FILE;
+
 @Component
 @ViewScoped
 @Getter
@@ -41,6 +48,28 @@ public class ProjectDetailBean extends PrimefacesBean {
 
     public void setFile(UploadedFile file) {
         this.file = file;
+    }
+
+    public List<String> getAvailableProjects() {
+        List<String> projects = new ArrayList<>();
+
+        if (this.toolsModelService.getCustomProperties().getStorageType().equals(STORAGE_TYPE_FILE)) {
+            Path path = Paths.get(this.toolsModelService.getCustomProperties().getStorageDirectory());
+
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+                for (Path file : stream) {
+                    String name = file.getName(file.getNameCount()-1).toString();
+
+                    if (name.endsWith(".json")) {
+                        projects.add(name.replace(".json", ""));
+                    }
+                }
+            } catch (IOException | DirectoryIteratorException ex) {
+                System.err.println("Error: " + ex.getMessage());
+            }
+        }
+
+        return projects;
     }
 
     /*public void handleFileUpload(FileUploadEvent event) {
